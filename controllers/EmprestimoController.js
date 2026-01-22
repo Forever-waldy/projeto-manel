2ba1da222bf3af6aae13b3954e227bb19d9e0138
SwagMessiah02 <vmdemesquita@gmail.com>
2026-01-22 13:33:00 -0300

    refactor(EmprestimoController.js): Separando rotas de acesso para /emprestimos

---
+++ controllers/EmprestimoController.js
@@ -0,0 +1,47 @@
+const EmprestimoService = require('../services/EmprestimosService.js');
+const AmigoService = require('../services/AmigoService.js');
+const JogosService = require('../services/JogosService.js');
+const {Emprestimo, Amigo, Jogo} = require('../models');
+
+class EmprestimoController {
+    constructor () {
+        this.emprestimoService = new EmprestimoService(Emprestimo);
+        this.amigoService = new AmigoService(Amigo);
+        this.jogosService = new JogosService(Jogo);
+    }
+
+    // /emprestimos
+    exibirEmprestimos = async (req, res) => {
+        const emprestimos = await this.emprestimoService.getAllEmprestimos();
+
+        res.render('emprestimos/index', { emprestimos });
+    }
+
+    // /emprestimos/novo
+    exibirCriarEmprestimo = async (req, res) => {
+        const jogos = await this.jogosService.getAllJogosOrderedByTitle();
+        const amigos = await this.amigoService.getAllAmigosOrderedByName();
+        res.render('emprestimos/novo', { jogos, amigos });
+    }
+
+    // /emprestimos/novo
+    criarEmprestimo = async (req, res) => {
+        const { jogoId, amigoId, dataInicio, dataFim } = req.body;
+        await this.emprestimoService.createEmprestimo({
+            jogoId: Number(jogoId),
+            amigoId: Number(amigoId),
+            dataInicio,
+            dataFim: dataFim || null
+        });
+
+        res.redirect('/emprestimos');
+    }
+
+    // /emprestimos/excluir/:id
+    excluirEmprestimo = async (req, res) => {
+        await this.emprestimoService.excluirEmprestimo({ where: { id: req.params.id } });
+        res.redirect('/emprestimos');
+    }
+}
+
+module.exports = EmprestimoController;
